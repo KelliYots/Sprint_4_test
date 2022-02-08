3e766f1b5bb2a8b9fec63fbce6a7afbc26c81b07
Ariniziya <murchelot@yandex.ru>
2022-02-08 21:01:17 +0300

    add test program class

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,2 +1,50 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class AccountTest {
+
+    private final String name;
+    private final Boolean expectedName;
+
+    public AccountTest(String name, boolean expectedName) {
+        this.name = name;
+        this.expectedName = expectedName;
+    }
+
+    // TODO: 08.02.2022 напиши позже нормальные граничные значения, плез
+    @Parameterized.Parameters
+    public static Object[] dataForTest() {
+        return new Object[][]{
+                {"Йо", false},
+                {"Йо ", false},
+                {"Й О", true},
+                {"", false},
+                {null, false},
+                {"Людвиг Витгенштейн", true},
+                {"ЛюдвигЙо Витгенштейн", false},
+                {"Людвиг Витгенштейн", true},
+                {" Людвиг Витгенштейн", false},
+                {"Людвиг Витгенштейн ", false},
+                {" ЛюдвигВитгенштейн ", false},
+                {" ЛюдвигВитгенштейн", false},
+                {"ЛюдвигВитгенштейн ", false},
+                {"Людвиг  Витгенштейн", false},
+        };
+    }
+
+    @Test
+    @DisplayName("Проверка валидности имени держателя карты")
+    @Description("Проверка соответствия имени ограничениям ввода")
+    public void NameValidationsTest() {
+        Account account = new Account(name);
+        boolean actual = account.checkNameToEmboss();
+        assertEquals(expectedName, actual);
+
+    }
 }
